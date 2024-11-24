9af4e157567a6e0fa9764aa86bdc121f4b4ab239
AYAKA-de <ayakadesignoffice@gmail.com>
2024-11-24 21:16:16 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP KADAI_007</title>
+</head>
+<body>
+  <?php
+// 連想配列
+$food_data = ['name' => 'onion', 'price' => 200,  'weight' => 160,];
+
+// 連想配列の値を出力する
+print_r($food_data);
+
+?>
+
+</body>
+</html>
